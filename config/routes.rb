207dcc4ed9a5fd6259937201d253ef1ac0e93cbf
StaZarycki <stazarycki@gmail.com>
2025-11-25 21:40:55 +0100

    feat: devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, path: "secure"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "pages#home"
+  root to: "pages#home"
 end
