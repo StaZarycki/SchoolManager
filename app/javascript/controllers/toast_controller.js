8630b63920615c369cf597ba6ce431f6b55aadba
StaZarycki <stazarycki@gmail.com>
2025-11-26 17:19:28 +0100

    feat: navbar

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -1,7 +1,27 @@
 import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
+    connect() {
+        this.timeoutId = setTimeout(() => {
+            this.close()
+        }, 5000)
+    }
+
     close() {
-        this.element.remove()
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId)
+        }
+
+        this.element.classList.add('opacity-0', 'transition-opacity', 'duration-300')
+
+        this.element.addEventListener('transitionend', () => {
+            this.element.remove()
+        }, {once: true})
+    }
+
+    disconnect() {
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId)
+        }
     }
 }
