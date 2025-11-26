3ae92f237c12013330d374c5649bf2cb20d3575a
StaZarycki <stazarycki@gmail.com>
2025-11-26 17:19:36 +0100

    feat: user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum :role, {
+    student: 0,
+    teacher: 1,
+    administrator: 2
+  }
+
+  validates :role, presence: true
 end
